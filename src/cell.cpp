1d9bfdc86a8be6b66e695e814e6fe3eb0e6127dd
bag-man <garland.owen@gmail.com>
2015-03-01 16:10:57 +0000

    Added cell.cpp

---
+++ src/cell.cpp
@@ -0,0 +1,14 @@
+#include "cell.h"
+
+using namespace std;
+
+list<Aphid> aphids;
+list<Ladybird> ladybirds;
+
+Cell::Cell(int aphidCount, int ladybirdCount) {
+
+}
+
+void update(Cell * cell, Board * board) {
+
+}
