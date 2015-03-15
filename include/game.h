362a67dfec04bba6ecb2f961504d7b13a14b5b85
bag-man <garland.owen@gmail.com>
2015-03-15 14:16:43 +0000

    Cleaner

--- include/game.h
+++ include/game.h
@@ -1,5 +1,8 @@
 #ifndef GAME_H
 #define GAME_H
+#include <fstream>
+#include <iostream>
+#include <unistd.h> // Unix only
 #include "board.h"
 #include "aphid.h"
 #include "ladybird.h"
--- src/game.cpp
+++ src/game.cpp
@@ -1,6 +1,3 @@
-#include <fstream>
-#include <iostream>
-#include <unistd.h> // Unix only
 #include "game.h"
 
 using namespace std;
